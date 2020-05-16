f4a1a5f5ec1868c3e4d49c7ae5a36b3c3eeeeb59
theoleehusky <theoleehusky@gmail.com>
2020-05-16 19:42:59 +0800

    Cleaned up organization. Apparently I can't publicize my tokens.

--- code/bot.py
+++ code/bot.py
@@ -4,6 +4,10 @@ import requests
 import time
 import asyncio
 from bs4 import BeautifulSoup
+import os
+import json
+
+path = os.getcwd()
 
 
 class MyClient(discord.Client):
@@ -13,7 +17,6 @@ class MyClient(discord.Client):
 
     async def on_ready(self):
         print('Logged in as', self.user.name)
-        print('------')
         await client.change_presence(activity=discord.Activity(name='you', type=discord.ActivityType.watching))
 
     async def on_message(self, message):
@@ -35,7 +38,7 @@ class MyClient(discord.Client):
         if re.search(" no you | no u ", message.content):
             await message.channel.send("no u")
         if re.search("schedule", message.content):
-            await message.channel.send(file=discord.File('schedule.png'))
+            await message.channel.send(file=discord.File('references/schedule.png'))
         if re.search(" ap ", message.content):
             await message.channel.send(file=discord.File('AP.jpg'))
         if re.search("what day", message.content):
@@ -47,25 +50,7 @@ class MyClient(discord.Client):
         if message.content == " school bell ":
             await self.youtube(message.author.voice.channel, 'bell')
 
-    # async def my_background_task(self):
-    #     return
-    #     await self.wait_until_ready()
-    #     server0 = client.get_guild(694184620811485284)
-    #     server1 = client.get_guild(696207735590486086)
-    #     while True:
-    #         await asyncio.sleep(1)
-    #         t = time.localtime()
-    #         if 0 <= t.tm_wday <= 4 and t.tm_sec in [0, 1]:
-    #             if [t.tm_hour, t.tm_min] in [[8, 0], [8, 25], [9, 0], [9, 10], [9, 15], [10, 15], [10, 25], [10, 30], [11, 30], [12, 25], [12, 30], [13, 30], [13, 40], [13, 45], [14, 45]]:
-    #                 for channel in server0.voice_channels + server1.voice_channels:
-    #                     if channel.members:
-    #                         try:
-    #                             await self.youtube(channel, 'bell')
-    #                             print(channel, end=': ')
-    #                             print(t.tm_hour, t.tm_min)
-    #                         except:
-    #                             print(channel)
-    #                             print(channel.members)
+    async def my_background_task(self):
 
     async def youtube(self, channel, key):
         voice = await channel.connect()
@@ -105,5 +90,9 @@ for day in range(104, 367):
         n = n % 4
 
 
+with open(os.path.join(path, 'code/references/token.txt'), 'r') as f:
+    token = json.load(f)
+
 client = MyClient()
-client.run('Njk1MDk0NDMzMjcxNzc1MzMy.XoVM3g.1z2LIpaH22gZTQBuHBuzbNvgfjQ')
+client.run(token)
+
